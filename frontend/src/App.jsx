5eced9798ee5f8897a8e7de18c7a2a34c6fb4d8d
aryansharma2k4 <aryansharma2k4@gmail.com>
2025-02-11 13:17:54 +0530

    Initialized a project using react in frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="">Hello welcome to Book My Course</p>
+    </>
+  )
+}
+
+export default App
